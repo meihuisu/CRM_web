ef407c2ef6654e1eec7d40484ca7933046a443af
Mei-Hui Su <mei@MeiPro.local>
2020-08-25 17:22:19 -0700

    ctm file going into shp converter need to have 'id' field too https://mygeodata.cloud/converter/latlong-to-shp

--- schema/crm/scripts/convert-shp.sh
+++ schema/crm/scripts/convert-shp.sh
@@ -8,6 +8,9 @@ pwd=`pwd`
 #CRM_db
 shp2pgsql -I -s GCS_WGS_1984 $pwd/../CRM_polygons_domains/CRM_polygons_domains crm_trace_tb > CRM_polygon_traces.sql
 
+# old one
+#shp2pgsql -I -s GCS_WGS_1984 $pwd/../CTM_polygons_2020/ctm_polygon-point ctm_point_trace_tb > CTM_old_point_traces.sql
+
 #CTM_db
 shp2pgsql -I -s GCS_WGS_1984 $pwd/../CTM_points_2020/ctm-point ctm_point_trace_tb > CTM_point_traces.sql
 
